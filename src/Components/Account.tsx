309b6703969fee7d8d7f5fa06a7726b22d7f08ca
yelin <juno2020k@gmail.com>
2024-01-19 19:23:35 +0900

    프로필 디자인

--- src/Components/Account.tsx
+++ src/Components/Account.tsx
@@ -1,16 +1,35 @@
 import React from "react";
-
+import "../Styles/Accountstyle.css";
 function Account(){
     return(
        <div>
-        <h1>Account</h1>
-        <h2>NeatFin에서 나를 표현하고 NeatFin을 보는 방식을 선택하세요</h2>
+        <h3>Account</h3>
         <hr />
-        <h2>내 Neatfin채널</h2>
-        <p>채널은 NeatFin에 공개되는 나의 공간입니다.
-             이미지을 업로드하고, 글에 댓글을 달고, 소비차트를 만들려면 채널이 필요합니다. </p>
-        <h2>내채널</h2>
-        <img src="" alt="googleaccountimg" /> 이름
+        <form action = " " className="AccountContainer">
+        <h3>NeatFin에서 나를 표현하고 NeatFin을 보는 방식을 선택하세요</h3>
+        <hr />
+        <h3>프로필 이미지</h3>
+        <img className="profileimg" src="https://cdn.inflearn.com/public/main/profile/default_profile.png" alt="googleaccountimg" />
+        <button className="Accountimgbtn">변경</button>
+        <p>확장자: png, jpg, jpeg/ 용량: 1MB 이하</p>
+        <h3>닉네임</h3>
+        <p>한글, 영문(대소문자), 숫자 조합 /2 - 18자 이하</p>
+        <input className="nickinput" type="text" />
+        <h3>자기소개</h3>
+        <textarea id="myTextArea" rows={5} cols={50}></textarea>
+        <br />
+        <button className="savebtn">저장하기</button>
+        </form>
+        <div className="AccountContainer">
+            <h3>이메일</h3>
+            <input className="nickinput" type="text" />
+            <button className="savebtn">저장하기</button>
+
+        </div>
+        <div className="AccountContainer">
+        <h3>탈퇴</h3>
+        </div>
+        
        </div>
 
 
--- src/Pages/MyPage.tsx
+++ src/Pages/MyPage.tsx
@@ -32,9 +32,9 @@ function Mypage(){
         <div style={{ display: 'flex' }}>
         <div className='myaside'>
         <h1>MyPage</h1>
-        <button className="mybtn" onClick={() => setCurrentPage('information')}>정보 수정</button>
+        <button className="mybtn" onClick={() => setCurrentPage('information')}>계정정보</button>
         <button  className="mybtn" onClick={() => setCurrentPage('settings')}>설정</button> 
-        <button  className="mybtn" onClick={() => setCurrentPage('posts')}>내가 작성한 게시글</button>
+        <button  className="mybtn" onClick={() => setCurrentPage('posts')}>작성한 게시글</button>
        
         </div>             
         <div style={{ flex: 1, padding: '10px' }}>
